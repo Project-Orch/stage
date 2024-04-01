ac9511062cc0254e7b7b49281de925c128e64fac
Futheme <kyoro_souza@hotmail.com>
2024-04-01 20:58:47 -0300

    🚧 container for components on storybook

---
+++ packages/react/src/stories/components/Container.tsx
@@ -0,0 +1,22 @@
+import { css } from '../../../styled-system/css'
+import { ReactNode } from 'react'
+
+interface IContainer {
+  children: ReactNode | ReactNode[]
+}
+
+export const Container = ({ children }: IContainer) => {
+  return (
+    <div
+      className={css({
+        display: 'flex',
+        flex: 1,
+        height: '100%',
+        width: '100%',
+        gap: '1rem',
+      })}
+    >
+      {children}
+    </div>
+  )
+}
