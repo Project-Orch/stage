a68db20ccf8dc3bf825522222a9cec12f5c2ec87
Futheme <kyoro_souza@hotmail.com>
2024-05-24 22:13:41 -0300

    ✨ new icon component

---
+++ packages/react/src/assets/icons/components/SimpleSidebar.tsx
@@ -0,0 +1,13 @@
+/* GENERATED FILE */
+import React, { forwardRef } from 'react'
+import type { Icon } from '../types'
+import IconBase from '../base'
+import weights from '../defs/SimpleSidebar'
+
+// @ts-ignore
+const I: Icon = forwardRef((props, ref) => (
+  <IconBase ref={ref} {...props} weights={weights} />
+))
+
+I.displayName = 'SidebarSimple'
+export { I as SidebarSimple }
