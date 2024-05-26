0249284b4c731be84e14329817ce9cfc6c6ee55c
Futheme <kyoro_souza@hotmail.com>
2024-05-26 16:17:30 -0300

    ✨ new user icon

---
+++ packages/react/src/assets/icons/components/User.tsx
@@ -0,0 +1,13 @@
+/* GENERATED FILE */
+import React, { forwardRef } from 'react'
+import type { Icon } from '../types'
+import IconBase from '../base'
+import weights from '../defs/User'
+
+// @ts-ignore
+const I: Icon = forwardRef((props, ref) => (
+  <IconBase ref={ref} {...props} weights={weights} />
+))
+
+I.displayName = 'User'
+export { I as User }
